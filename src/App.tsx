22fb9797bf2d2792ad0a505b14a6e69ad1ead0ff
Mustafa Saifee <saifee.mustafa@gmail.com>
2025-08-28 23:58:20 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { H3 } from '@undp/design-system-react';
+
+import '@/styles/fonts.css';
+import '@/styles/style.css';
+import '@undp/data-viz/style.css';
+import '@undp/design-system-react/style.css';
+import undpLogo from './assets/undp-logo-blue.svg';
+
+function App() {
+  return (
+    <div className='undp-container'>
+      <div className='m-5'>
+        <div>
+          <img
+            src={undpLogo}
+            className='logo react mb-8'
+            alt='React logo'
+            width='72px'
+            style={{ marginLeft: 'auto', marginRight: 'auto' }}
+          />
+        </div>
+        <H3 className='text-center'>
+          This is template for building visualization and frontend project
+          maintained by UNDP&apos;s SDG Integration Team.
+          <br />
+          <br />
+          Contact us at data@undp.org if you have any feedback or questions.
+        </H3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,92 @@
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from '@nabla/vite-plugin-eslint';
+import tailwindcss from '@tailwindcss/vite';
+import { AtRule } from 'postcss';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    eslint(),
+    tailwindcss(),
+    visualizer({ filename: 'stats.html', open: true }),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'remove-layers', // If you want to remove base layer
+          AtRule: {
+            layer(rule) {
+              if (rule.params !== 'base') {
+                rule.each(child => {
+                  rule.parent.insertAfter(rule, child);
+                });
+                rule.remove();
+              }
+            },
+          },
+        },
+        {
+          postcssPlugin: 'wrap-with-undp-container', // If you want to wrap all the class in .undp-container
+          Once(root) {
+            const skipSelectors = ['html', 'body', ':root', ':host'];
+            root.walkRules(rule => {
+              if (rule.parent && rule.parent.type === 'atrule') {
+                const parent = rule.parent as AtRule;
+                if (parent.name === 'keyframes' || parent.name === 'supports') {
+                  return;
+                }
+              }
+              rule.selectors = rule.selectors.map(selector => {
+                if (selector.startsWith('.undp-container')) return selector;
+                if (skipSelectors.some(skip => selector.startsWith(skip)))
+                  return selector;
+                return `.undp-container ${selector}`;
+              });
+            });
+          },
+        },
+        {
+          postcssPlugin: 'move-media-queries-last', // If you want to reorder media queries to the end
+          Once(root) {
+            const mediaQueries = [];
+
+            // Collect all media queries
+            root.walkAtRules('media', mediaRule => {
+              mediaQueries.push(mediaRule.clone());
+              mediaRule.remove();
+            });
+
+            // Append them at the end
+            mediaQueries.forEach(mediaQuery => {
+              root.append(mediaQuery);
+            });
+          },
+        },
+      ],
+    },
+  },
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+      treeshake: true,
+    },
+  },
+  server: {
+    cors: {
+      origin: '*',
+      methods: ['GET'],
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+});
