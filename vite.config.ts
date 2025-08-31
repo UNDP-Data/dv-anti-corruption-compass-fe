29ae743bff6ee7e678c038faf5acbd85b1f2c5a7
Mustafa Saifee <mustafa.saifee@undp.org>
2025-08-31 19:28:29 +0300

    Fix ci cd

--- vite.config.ts
+++ vite.config.ts
@@ -81,6 +81,7 @@ export default defineConfig({
     cssCodeSplit: false,
     rollupOptions: {
       output: {
+        manualChunks: undefined,
         entryFileNames: '[name].js',
         assetFileNames: '[name].[ext]',
       },
