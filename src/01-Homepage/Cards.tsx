1a958839a78fcc39d81c0ecbf69900d53cacba95
Mustafa Saifee <mustafa.saifee@undp.org>
2025-09-26 09:40:47 +0300

    Update Cards.tsx

--- src/01-Homepage/Cards.tsx
+++ src/01-Homepage/Cards.tsx
@@ -13,23 +13,25 @@ interface Props {
 }
 
 export const CardEl = ({ img, title, date }: Props) => (
-  <Card
-    border
-    size='sm'
-    variant='with-image'
-    className='rounded-[20px] border-0 max-w-65'
-  >
-    <CardHeader>
-      <CardImage
-        className='rounded-tl-[20px] rounded-tr-[20px] h-40'
-        src={img}
-      />
-      <CardTitle className='poppins-medium !text-[16px] !leading-[26px]'>
-        {title}
-      </CardTitle>
-      <CardDescription className='poppins-regular !text-[12px] !leading-[16px] text-[#808191]'>
-        Published on {date}
-      </CardDescription>
-    </CardHeader>
-  </Card>
+  <div className='dark'>
+    <Card
+      border
+      size='full'
+      variant='with-image'
+      className='rounded-[20px] border-0 max-w-65'
+    >
+      <CardHeader>
+        <CardImage
+          className='rounded-tl-[20px] rounded-tr-[20px] h-40'
+          src={img}
+        />
+        <CardTitle className='poppins-medium !text-[16px] !leading-[26px]'>
+          {title}
+        </CardTitle>
+        <CardDescription className='poppins-regular !text-[12px] !leading-[16px] text-[#808191]'>
+          Published on {date}
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  </div>
 );
