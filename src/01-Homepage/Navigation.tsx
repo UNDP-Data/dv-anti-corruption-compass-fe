31245174bb5c77bf1108b39b7585306fd06f7e05
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2025-10-14 14:52:47 +0300

    Update navigation element

--- src/01-Homepage/Navigation.tsx
+++ src/01-Homepage/Navigation.tsx
@@ -7,7 +7,6 @@ interface Props {
   isInViewSlideThree: boolean;
   refControlPanelOne: React.RefObject<HTMLDivElement | null>;
   refControlPanelTwo: React.RefObject<HTMLDivElement | null>;
-  refControlPanelThree: React.RefObject<HTMLDivElement | null>;
   refSlideThree: React.RefObject<HTMLDivElement | null>;
 }
 
@@ -27,7 +26,6 @@ const Navigation = (props: Props) => {
     isInViewSlideThree,
     refControlPanelOne,
     refControlPanelTwo,
-    refControlPanelThree,
     refSlideThree,
   } = props;
   return (
@@ -37,65 +35,48 @@ const Navigation = (props: Props) => {
       <div className='flex gap-2 items-center'>
         <div className='flex items-center h-4 w-20 text-right'>
           <P
-            className='poppins-regular !text-[12px] text-right text-[#fff] w-20 opacity-0 md:opacity-100'
+            className={`poppins-regular !text-[12px] text-right text-[#fff] w-20 opacity-0 md:opacity-100 ${!isInViewControlPanelOne ? 'hidden' : ''}`}
             marginBottom='none'
           >
             Public procurement integrity
           </P>
         </div>
         <div
-          className={`cursor-pointer rounded-full w-4 h-4 border-2 border-[#fff] ${isInViewControlPanelOne ? 'bg-[#fff]' : 'bg-[rgba(255,255,255,0.3)]'} hover:!bg-[rgba(255,255,255,0.5)`}
+          className={`cursor-pointer rounded-full w-5 h-5 border-1 border-[#fff] ${isInViewControlPanelOne ? 'bg-[#fff]' : 'bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.5),rgba(7,43,78,0.5))]'}`}
           onClick={() => {
             handleScroll(refControlPanelOne);
           }}
         />
       </div>
-      <div className='ml-22 w-[1px] h-10 bg-[#fff]' />
+      <div className='ml-22 my-2 w-[1px] h-10 border-l border-dashed border-white' />
       <div className='flex gap-2 items-center'>
         <div className='flex items-center h-4 w-20 text-right'>
           <P
-            className='poppins-regular !text-[12px] text-right text-[#fff] w-20 opacity-0 md:opacity-100'
+            className={`poppins-regular !text-[12px] text-right text-[#fff] w-20 opacity-0 md:opacity-100 ${!isInViewControlPanelTwo ? 'hidden' : ''}`}
             marginBottom='none'
           >
             Business experience
           </P>
         </div>
         <div
-          className={`cursor-pointer rounded-full w-4 h-4 border-2 border-[#fff] ${isInViewControlPanelTwo ? 'bg-[#fff]' : 'bg-[rgba(255,255,255,0.3)]'} hover:!bg-[rgba(255,255,255,0.5)`}
+          className={`cursor-pointer rounded-full w-5 h-5 border-1 border-[#fff] ${isInViewControlPanelTwo ? 'bg-[#fff]' : 'bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.5),rgba(7,43,78,0.5))]'}`}
           onClick={() => {
             handleScroll(refControlPanelTwo);
           }}
         />
       </div>
-      <div className='ml-22 w-[1px] h-10 bg-[#fff]' />
+      <div className='ml-22 my-2 w-[1px] h-10 border-l border-dashed border-white' />
       <div className='flex gap-2 items-center'>
         <div className='flex items-center h-4 w-20 text-right'>
           <P
-            className='poppins-regular !text-[12px] text-right text-[#fff] w-20 opacity-0 md:opacity-100'
-            marginBottom='none'
-          >
-            Anti-corruption authorities
-          </P>
-        </div>
-        <div
-          className={`cursor-pointer rounded-full w-4 h-4 border-2 border-[#fff] ${isInViewControlPanelThree ? 'bg-[#fff]' : 'bg-[rgba(255,255,255,0.3)]'} hover:!bg-[rgba(255,255,255,0.5)`}
-          onClick={() => {
-            handleScroll(refControlPanelThree);
-          }}
-        />
-      </div>
-      <div className='ml-22 w-[1px] h-10 bg-[#fff]' />
-      <div className='flex gap-2 items-center'>
-        <div className='flex items-center h-4 w-20 text-right'>
-          <P
-            className='poppins-regular !text-[12px] text-right text-[#fff] w-20 opacity-0 md:opacity-100'
+            className={`poppins-regular !text-[12px] text-right text-[#fff] w-20 opacity-0 md:opacity-100 ${!isInViewSlideThree || isInViewControlPanelTwo ? 'hidden' : ''}`}
             marginBottom='none'
           >
             Country level insights
           </P>
         </div>
         <div
-          className={`cursor-pointer rounded-full w-4 h-4 border-2 border-[#fff] ${isInViewSlideThree ? 'bg-[#fff]' : 'bg-[rgba(255,255,255,0.3)]'} hover:!bg-[rgba(255,255,255,0.5)`}
+          className={`cursor-pointer rounded-full w-5 h-5 border-1 border-[#fff] ${isInViewSlideThree && !isInViewControlPanelTwo ? 'bg-[#fff]' : 'bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.5),rgba(7,43,78,0.5))]'}`}
           onClick={() => {
             handleScroll(refSlideThree);
           }}
