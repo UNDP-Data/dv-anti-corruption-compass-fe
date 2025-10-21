fede53815c52bdc53d5154e125ec7e69b1c4a8d6
Mustafa Saifee <mustafa.saifee@undp.org>
2025-10-21 17:00:25 +0300

    Update DataTable.tsx

--- src/01-Homepage/Components/DataTable.tsx
+++ src/01-Homepage/Components/DataTable.tsx
@@ -6,6 +6,12 @@ import { Link } from '@tanstack/react-router';
 import { Spacer } from '@undp/design-system-react/Spacer';
 import { useState } from 'react';
 import { getTextColorBasedOnBgColor } from '@undp/data-viz/utils';
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@undp/design-system-react/HoverCard';
+import { InfoIcon } from 'lucide-react';
 
 import {
   DROPDOWN_CLASSNAMES_MULTI_SELECT,
@@ -14,6 +20,7 @@ import {
 } from '@/Constants';
 import { DataType, PillarsMetaDataType } from '@/Types';
 import { customDropdownComponents } from '@/Utils/DropdownComponents';
+import { ParagraphText } from '@/Components/Typography';
 
 interface Props {
   data: DataType[];
@@ -28,61 +35,80 @@ function DataTable({ data, pillarsMetaData }: Props) {
   const subPillars = pillarsMetaData.map(d => d.subPillars).flat();
   return (
     <div className='gap-4.5 flex flex-col w-full text-primary-gray-700'>
-      <div className='gap-4 flex'>
-        <div className='flex flex-col gap-1 w-[calc(25%-0.75rem)] grow-1 min-w-[240px] max-w-[480px] flex-wrap'>
-          <Label className='text-primary-white'>Filter by year</Label>
-          <DropdownSelect
-            value={{
-              value: selectedYear,
-              label: selectedYear,
-            }}
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onChange={(d: any) => {
-              setSelectedYear(d.value);
-            }}
-            placeholder='Select contract value'
-            options={YEARS.map(d => ({
-              value: d,
-              label: d,
-            }))}
-            size='base'
-            variant='normal'
-            className='bg-primary-white! border-0! rounded-[8px]!'
-            classNames={DROPDOWN_CLASSNAMES_WHITE}
-            isClearable={false}
-            components={customDropdownComponents('light', false)}
-          />
-        </div>
-        <div className='flex flex-col gap-1 w-[calc(25%-0.75rem)] grow-1 min-w-[240px] max-w-[480px] flex-wrap'>
-          <Label className='text-primary-white'>Filter by pillar</Label>
-          <DropdownSelect
-            placeholder='Select Pillar'
-            value={selectedPillars.map(d => ({
-              value: d,
-              label: d,
-            }))}
-            isMulti
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onChange={(d: any) => {
+      <div className='flex justify-between w-full items-center'>
+        <div className='gap-4 flex grow-1'>
+          <div className='flex flex-col gap-1 w-[calc(25%-0.75rem)] grow-1 min-w-[240px] max-w-[480px] flex-wrap'>
+            <Label className='text-primary-white'>Filter by year</Label>
+            <DropdownSelect
+              value={{
+                value: selectedYear,
+                label: selectedYear,
+              }}
               // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              setSelectedPillars(d.map((el: any) => el.value));
-            }}
-            options={pillarsMetaData.map(d => ({
-              label: d.value,
-              options: d.subPillars.map(el => ({
-                value: el.value,
-                label: el.value,
-              })),
-            }))}
-            size='base'
-            variant='normal'
-            className='bg-primary-white! border-0! rounded-[8px]!'
-            classNames={DROPDOWN_CLASSNAMES_MULTI_SELECT}
-            isClearable={false}
-            components={customDropdownComponents('light', true)}
-            maxTagCount={2}
-          />
+              onChange={(d: any) => {
+                setSelectedYear(d.value);
+              }}
+              placeholder='Select contract value'
+              options={YEARS.map(d => ({
+                value: d,
+                label: d,
+              }))}
+              size='base'
+              variant='normal'
+              className='bg-primary-white! border-0! rounded-[8px]!'
+              classNames={DROPDOWN_CLASSNAMES_WHITE}
+              isClearable={false}
+              components={customDropdownComponents('light', false)}
+            />
+          </div>
+          <div className='flex flex-col gap-1 w-[calc(25%-0.75rem)] grow-1 min-w-[240px] max-w-[480px] flex-wrap'>
+            <Label className='text-primary-white'>Filter by pillar</Label>
+            <DropdownSelect
+              placeholder='Select Pillar'
+              value={selectedPillars.map(d => ({
+                value: d,
+                label: d,
+              }))}
+              isMulti
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              onChange={(d: any) => {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                setSelectedPillars(d.map((el: any) => el.value));
+              }}
+              options={pillarsMetaData.map(d => ({
+                label: d.value,
+                options: d.subPillars.map(el => ({
+                  value: el.value,
+                  label: el.value,
+                })),
+              }))}
+              size='base'
+              variant='normal'
+              className='bg-primary-white! border-0! rounded-[8px]!'
+              classNames={DROPDOWN_CLASSNAMES_MULTI_SELECT}
+              isClearable={false}
+              components={customDropdownComponents('light', true)}
+              maxTagCount={2}
+            />
+          </div>
         </div>
+        <HoverCard openDelay={0}>
+          <HoverCardTrigger>
+            <div className='flex gap-2 items-center mt-6'>
+              <ParagraphText
+                weight='medium'
+                marginBottom='none'
+                className='p-0 leading-normal'
+              >
+                Data availability
+              </ParagraphText>
+              <InfoIcon color='#fff' size={16} />
+            </div>
+          </HoverCardTrigger>
+          <HoverCardContent className='rounded text-[12px] poppins-regular !leading-[150%] p-3 rounded-[8px] text-[#4D4D4D] w-60'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </HoverCardContent>
+        </HoverCard>
       </div>
       <Spacer size='lg' />
       <div className='dark'>
