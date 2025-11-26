937107ddf8b68c34e9f83716ff4c94d3898a513f
Mustafa Saifee <mustafa.saifee@undp.org>
2025-11-26 10:12:37 +0200

    Update Navigation.tsx

--- src/01-Homepage/Components/Navigation.tsx
+++ src/01-Homepage/Components/Navigation.tsx
@@ -19,7 +19,7 @@ const Navigation = (props: Props) => {
     indicatorsMetaData,
   } = props;
   return (
-    <div className='fixed z-20 flex flex-col gap-0 justify-center items-center right-8 top-[50%] transform-[translate(0, -50%) pointer-events-none'>
+    <div className='fixed z-20 flex flex-col gap-0 justify-center items-center right-8 top-[50%] transform-[translate(0, -50%)'>
       {indicatorsMetaData.map((d, i) => (
         <Fragment key={i}>
           <div className='flex gap-2 items-center'>
